aadc69a73f8aeb14f2706ac452c355dc7e3da605
Paulo Pereira <phfaverop@gmail.com>
2023-09-25 20:21:43 -0300

    remove unused comments

--- app/domain/entities/kadence/space.py
+++ app/domain/entities/kadence/space.py
@@ -5,20 +5,11 @@ from pydantic import BaseModel
 class Neighborhood(BaseModel):
     id: str
     name: str
-    # "@id": "/v1/public/neighborhoods/01H9TG9Z73XS6YZCC1CGTSDQFK",
-    # "@type": "Neighborhood",
-    # "id": "01H9TG9Z73XS6YZCC1CGTSDQFK",
-    # "name": "Engineering"
         
 
 class Floor(BaseModel):
         id: str
         name: str
-        #   "@id": "/v1/public/floors/01H9TG9Z6K07RY2S5NSPZ6R37Y",
-        #   "@type": "Floor",
-        #   "id": "01H9TG9Z6K07RY2S5NSPZ6R37Y",
-        #   "name": "Ground floor"
-
 
 class Space(BaseModel):
     id: str
