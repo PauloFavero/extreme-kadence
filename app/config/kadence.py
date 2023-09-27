bf7c5b7690b6d8c26685b728d804d0385bb01634
Paulo Pereira <phfaverop@gmail.com>
2023-09-27 06:42:49 -0300

    add kadence api config

--- app/config/kadence.py
+++ app/config/kadence.py
@@ -27,6 +27,19 @@ class KadenceSettings(BaseSettings):
         alias="kadence_password",
         validation_alias=AliasChoices("kadence_password", "kadence_pass"),
     )
+
+    base_uri: str = Field(
+        "https://api.onkadence.co",
+        alias="kadence_base_uri",
+        validation_alias=AliasChoices("kadence_base_uri", "kadence_base_url"),
+    )
+
+    api_version: str = Field(
+        "/v1/public",
+        alias="kadence_api_version",
+        validation_alias=AliasChoices("kadence_api_version", "kadence_version"),
+    )
+
     login_endpoint: str = Field(
         "https://login.onkadence.co/oauth2/token",
         alias="kadence_login_endpoint",
