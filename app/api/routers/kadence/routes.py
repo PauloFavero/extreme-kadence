7b608683ebf8e251ab3919e0a063ada19b8b5753
Paulo Pereira <phfaverop@gmail.com>
2023-09-27 06:44:11 -0300

    fetch bookings with pagination

--- app/api/routers/kadence/routes.py
+++ app/api/routers/kadence/routes.py
@@ -7,8 +7,14 @@ from typing import List
 import requests
 from fastapi import APIRouter, Depends
 from api.factories.kadence.auth_factory import kadence_auth_controller_factory
-from domain.entities.kadence.booking import Booking
+from domain.services.kadence.pagination import get_hydra_pagination
+from domain.entities.kadence.booking import (
+    Booking,
+    BookingList,
+    FetchBookingsFilterParams,
+)
 
+from config.kadence import KadenceSettings
 from domain.entities.kadence import KadenceAuthToken, CheckInMethod
 
 kadence_router = APIRouter(
@@ -16,9 +22,9 @@ kadence_router = APIRouter(
     tags=["Kadence Integration"],
 )
 
-BASE_URL = "https://api.onkadence.co/v1/public"
-
 auth_controller = kadence_auth_controller_factory()
+
+kadence_settings = KadenceSettings()
 
 
 @kadence_router.get("/auth", status_code=HTTPStatus.OK)
@@ -34,7 +40,7 @@ async def request_auth_token() -> KadenceAuthToken:
 def get_user(token: KadenceAuthToken = Depends(auth_controller.handle)):
     print(f"{datetime.now()} - GET /user")
     user = requests.get(
-        f"{BASE_URL}/users?email=phfaverop@gmail.com",
+        f"{kadence_settings.base_uri}{kadence_settings.api_version}/users?email=phfaverop@gmail.com",
         headers={"Authorization": f"{token.token_type} {token.access_token}"},
     )
     return user.json()
@@ -48,12 +54,28 @@ def get_user_bookings(
     user_id: str, token: KadenceAuthToken = Depends(auth_controller.handle)
 ) -> List[Booking]:
     print(f"{datetime.now()} - GET /users/{user_id}/bookings")
-    bookings = requests.get(
-        f"{BASE_URL}/users/{user_id}/bookings",
+    query_params = FetchBookingsFilterParams()
+    response = requests.get(
+        f"{kadence_settings.base_uri}{kadence_settings.api_version}/users/{user_id}/bookings",
         headers={"Authorization": f"{token.token_type} {token.access_token}"},
+        params=query_params.model_dump(by_alias=True),
     )
+    bookings = response.json()
+    pagination = get_hydra_pagination(bookings)
+    bookings_list = BookingList(itens=bookings.get("hydra:member", []))
 
-    return bookings.json().get("hydra:member", [])
+    ## fetch corresponding bookings
+    while pagination and pagination.next:
+        response = requests.get(
+            f"{kadence_settings.base_uri}{pagination.next}",
+            headers={"Authorization": f"{token.token_type} {token.access_token}"},
+            # params=query_params.model_dump(by_alias=True),
+        )
+        bookings = response.json()
+        pagination = get_hydra_pagination(bookings)
+        bookings_list.itens.extend(bookings.get("hydra:member", []))
+
+    return bookings_list.itens
 
 
 @kadence_router.post(
@@ -67,7 +89,7 @@ def checkin_user(
 ):
     print(f"{datetime.now()} - POST /user/bookings/{booking_id}/checkin")
     checkin = requests.post(
-        f"{BASE_URL}/bookings/{booking_id}/check-in",
+        f"{kadence_settings.base_uri}{kadence_settings.api_version}/bookings/{booking_id}/check-in",
         headers={"Authorization": f"{token.token_type} {token.access_token}"},
         json={"userId": user_id, "method": CheckInMethod.WIFI.value},
     )
