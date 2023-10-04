29605c03a81fbe23cb46fd3f5ef97fe0cd2780ea
Paulo Pereira <phfaverop@gmail.com>
2023-10-04 00:34:30 -0300

    fix python packages

--- app/server/factories/kadence/get_user_bookings_factory.py
+++ app/server/factories/kadence/get_user_bookings_factory.py
@@ -1,10 +1,11 @@
-from domain.services.kadence.booking import KadenceGetUserBookingsService
-from infra.http.kadence.bookings.get_user_bookings import GetUserBookingsRequester
+from infra.adapters.http.kadence.get_user_bookings import GetUserBookingsRequester
+from server.presentation.services.kadence.booking import KadenceGetUserBookingsService
 from config.server import environment
 
 kadence_config = environment.kadence
 
 
 def kadence_get_user_bookings_factory() -> KadenceGetUserBookingsService:
+    """Factory to get user bookings from Kadence API"""
     requester = GetUserBookingsRequester(kadence_config)
     return KadenceGetUserBookingsService(requester=requester, config=kadence_config)
