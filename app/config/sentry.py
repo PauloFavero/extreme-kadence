247c416c6441f065008d6fc8fddd166774a3d5bc
Paulo Pereira <phfaverop@gmail.com>
2023-10-06 02:20:57 -0300

    fix sentry baseSettings

--- app/config/sentry.py
+++ app/config/sentry.py
@@ -5,10 +5,7 @@ from pydantic_settings import BaseSettings
 class SentrySettings(BaseSettings):
     """Sentry Settings"""
 
-    dsn: str = Field(
-        "",
-        alias=AliasChoices("sentry_dsn", "sentry_url"),
-    )
+    dsn: str = Field(alias=AliasChoices("sentry_dsn", "sentry_url"))
     traces_sample_rate: float = Field(1.0, alias="sentry_traces_sample_rate")
     sample_rate: float = Field(1.0, alias="sentry_sample_rate")
     env: str = Field("production", alias="sentry_env")
